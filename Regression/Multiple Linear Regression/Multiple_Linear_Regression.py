ecb01b0a5b4c07a3f76bf324149983b336ad38d5
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2020-07-13 22:12:24 +0530

    Added prediction for single result in MLR

--- Regression/Multiple Linear Regression/Multiple_Linear_Regression.py
+++ Regression/Multiple Linear Regression/Multiple_Linear_Regression.py
@@ -29,3 +29,6 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#Making Single Predictions
+print(regressor.predict([[1, 0, 0, 160000, 130000, 300000]]))
